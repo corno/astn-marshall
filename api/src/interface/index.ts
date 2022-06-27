693cc578cb43ef8b8a8cbb83e4797e34d828ce17
corno <corno.astn.a@schraverus.com>
2022-06-27 17:14:49 +0200

    .

--- api/src/interface/index.ts
+++ api/src/interface/index.ts
@@ -1,4 +1,7 @@
 export * from "./interfaces/IMarshallableDataset"
+export * from "./interfaces/CreateASTNNormalizer"
+export * from "./interfaces/CreateAnnotater"
+export * from "./interfaces/CreateTreeParser"
 
 export * from "./types/SerializationStyle"
 export * from "./types/InternalSchemaSpecification"
